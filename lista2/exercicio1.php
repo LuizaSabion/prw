e781a576c32da2a5975e83c45e0ccb8642096b6b
luiza <marilusabion2203@gmail.com>
2022-03-30 11:26:44 -0300

    exercicio 1

---
+++ lista2/exercicio1.php
@@ -0,0 +1,13 @@
+<?php
+    /*.Faça um script PHP para somar os números pares < 100*/
+    
+    for ($num=1; $num <100 ; $num++) {
+        $num/2;
+        if ($num%2==0) {
+             echo $num."<br>";
+        }
+        
+       
+    }
+
+?>
