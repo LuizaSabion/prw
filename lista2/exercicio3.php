6c9562e7320850b9ae9057144a3a2b44ace8410d
luiza <marilusabion2203@gmail.com>
2022-03-30 11:36:34 -0300

    String

---
+++ lista2/exercicio3.php
@@ -0,0 +1,7 @@
+<?php
+        /*Um número é, por definição, primo se ele não tem divisores,
+exceto 1 e ele próprio. Preparar um script PHP para verificar
+números entre 0 e 200 e determinar se cada um dele é primo ou
+não. */
+
+?>
