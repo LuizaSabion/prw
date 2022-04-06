6c9dfd2a7d876763dec0b17d867c5063cea0816a
luiza <marilusabion2203@gmail.com>
2022-04-06 10:43:19 -0300

    exercicio4

--- lista3/exercicio4.php
+++ lista3/exercicio4.php
@@ -1,5 +1,14 @@
 <?php
         /* Crie um script em PHP que exiba separadamente o dia, mês
 e ano de uma data completa a partir de uma variável. */
+
     
+   $dia =  date('d')."<br>";
+   $mes = date('F')."<br>";
+   $ano = date('Y')."<br>";
+    
+    echo "Dia: ".$dia."<br>";
+    echo "Mês: ".$mes."<br>";
+    echo "Ano: ".$ano."<br>";
+    echo "hoje é:". date('d/m/y')."<br>";
 ?>
