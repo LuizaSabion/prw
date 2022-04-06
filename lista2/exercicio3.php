e0bab398018aea682b971730b7937b225da3255f
luiza <marilusabion2203@gmail.com>
2022-04-06 11:37:49 -0300

    exercicio3

--- lista2/exercicio3.php
+++ lista2/exercicio3.php
@@ -1,7 +1,20 @@
 <?php
-        /*Um número é, por definição, primo se ele não tem divisores,
-exceto 1 e ele próprio. Preparar um script PHP para verificar
-números entre 0 e 200 e determinar se cada um dele é primo ou
-não. */
-
+    /*Um número é, por definição, primo se ele não tem divisores,
+    exceto 1 e ele próprio. Preparar um script PHP para verificar
+    números entre 0 e 200 e determinar se cada um dele é primo ou
+    não. */
+    for ($num=1; $num <200 ; $num++){        
+       $primo=1; //parte do principio que o numero é primo
+       
+       for ($div=2 ; $div <= $num/2; $div++){     
+              
+            if ($num % $div == 0){
+                $primo = 0;
+                break;
+            }
+        }    
+        if ($primo == 1){
+            echo "O número ".$num ." é primo"."<br>";
+        }            
+    }
 ?>
