ac1d2859d136aa16ac37888b74c3f7d8f285e997
luiza <marilusabion2203@gmail.com>
2022-04-06 12:04:00 -0300

    exercicio3

--- lista3/exercicio3.php
+++ lista3/exercicio3.php
@@ -1,5 +1,10 @@
 <?php
-        /* Crie um script em PHP a em que a partir do seu CEP. Capture
-apenas os 3 últimos números e exiba-o. */
+    /* Crie um script em PHP a em que a partir do seu CEP. Capture
+    apenas os 3 últimos números e exiba-o. */
+
+    $cep = array('1','6','2','0','1','4','1','0');
+    sort($cep)
+
+
 
 ?>
