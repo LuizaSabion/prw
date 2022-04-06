c21db39c00bd06b9affc3e8456f0a280106981f2
luiza <marilusabion2203@gmail.com>
2022-04-06 09:47:23 -0300

    exercico 1

--- lista3/exercicio1.php
+++ lista3/exercicio1.php
@@ -2,9 +2,9 @@
         /*Crie um script em PHP que exiba quantos caracteres possui
 uma palavra ou frase que foi atribuída a uma vaiável.*/
 
-    $frase = "Percy Jackson e o Ladrão de raios ";
+    $frase = "Percy Jackson e o Ladrão de Raios";
 
-        echo $frase "<br>";
+        echo $frase."<br>";
 
         echo "A frase contém: " . strlen($frase) . "caracteres" . "<br>";
 
