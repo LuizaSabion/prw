9118512f570d7f94bac2f63f4f7a562829274d9d
luiza <marilusabion2203@gmail.com>
2022-04-06 09:00:54 -0300

    exercicio 2

--- lista2/exercicio2.php
+++ lista2/exercicio2.php
@@ -5,7 +5,46 @@ o mês lido. Caso o número digitado não esteja na faixa de 1 .. 12
 escreva uma mensagem informando o usuário do erro da
 digitação. */
 
-        $num= 5
+switch ($mes=2) {
+        
+        case 1:
+            echo "janeiro";
+            break;
+        case 2:
+            echo "fevereiro";
+            break;
+        case 3:
+            echo "março";
+            break;
+        case 4:
+            echo "abril";
+            break;
+        case 5:
+            echo "maio";
+            break;
+        case 6:
+            echo "junho";
+            break;
+        case 7:
+            echo "julho";
+            break;
+        case 8:
+            echo "agosto";
+            break;
+        case 9:
+            echo "setembro";
+            break;
+        case 10:
+            echo "outubro";
+            break;
+        case 11:
+            echo "novembro";
+            break;
+        case 12:
+            echo "dezembro";
+            break;
+        default:
+           echo "O numero informado não é valido";
         
 
 ?>
