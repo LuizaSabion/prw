73336365bdd74eaa1a684267119966cf2afb9cf0
luiza <marilusabion2203@gmail.com>
2022-04-06 11:52:44 -0300

    exercicio2

--- lista3/exercicio2.php
+++ lista3/exercicio2.php
@@ -1,5 +1,11 @@
 <?php
         /*Crie um script em PHP que pegue uma palavra contida em
-uma variável e substitua todas as vogais por X.*/
+        uma variável e substitua todas as vogais por X.*/
+        
+        $palavra = "instituto";
+        echo $palavra."<br>";
+        $vogal = array('a','e','i','o','u');
+        $troca = str_replace($vogal, "x", $palavra);
+        echo $troca;
 
 ?>
