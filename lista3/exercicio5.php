bfc1fddf1ff6eebc4ab2913248213d8994681c59
LuizaSabion <101723637+LuizaSabion@users.noreply.github.com>
2022-04-09 17:19:08 -0300

    Update exercicio5.php

--- lista3/exercicio5.php
+++ lista3/exercicio5.php
@@ -1,5 +1,19 @@
 <?php 
         /*Crie um script em PHP que a partir de uma data exiba o dia
-da semana (segunda, terça, ...).*/
+        da semana (segunda, terça, ...).*/
+
+       
+        $hoje = date("w");
+        $diaSemana[0] = "Domingo";
+        $diaSemana[1] = "Segunda-feira";
+        $diaSemana[2] = "Terça-feira";
+        $diaSemana[3] = "Quarta-feira";
+        $diaSemana[4] = "Quinta-feira";
+        $diaSemana[5] = "Sexta-feira";
+        $diaSemana[6] = "Sábado";
+        
+        echo "Hoje é: $diaSemana[$hoje]";
+        
+
 
 ?>
