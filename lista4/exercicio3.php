837e3cd32a329dec60e36f39019a7906b1579c1b
luiza <marilusabion2203@gmail.com>
2022-04-09 18:49:54 -0300

    exercicio 3

--- lista4/exercicio3.php
+++ lista4/exercicio3.php
@@ -2,7 +2,7 @@
     /* Escreva um programa em PHP para receber o nome de um estado do Brasil em
 um formulário e exibir sua capital.*/
 
-    switch ($sigla="AP") {
+    switch ($sigla="SP") {
             
         case 'AC':
             echo "Acre";
@@ -16,9 +16,77 @@ um formulário e exibir sua capital.*/
         case 'AM':
             echo "Amazonas";
             break;
-        
+        case 'BA':
+            echo "Bahia";
+            break;
+        case 'CE':
+            echo "Ceará";
+            break;
+        case 'DF':
+            echo "Distrito Federal";
+            break;
+        case 'ES':
+            echo "Espírito Santo";
+            break;
+        case 'GO':
+            echo "Goiás";
+            break;
+        case 'MA':
+            echo "Maranhão";
+            break;
+        case 'MT':
+            echo "Mato Grosso";
+            break;
+        case 'MS':
+            echo "Mato Grosso do Sul	";
+            break;
+        case 'MG':
+            echo "Minas Gerais";
+            break;
+        case 'PA':
+            echo "Pará";
+            break;
+        case 'PB':
+            echo "Paraíba";
+            break;
+        case 'PR':
+            echo "Paraná";
+            break;
+        case 'PE':
+            echo "Pernambuco";
+            break;
+        case 'PI':
+            echo "Piauí";
+            break;
+        case 'RJ':
+            echo " Rio de Janeiro";
+            break;
+        case 'RN':
+            echo "Rio Grande do Norte";
+            break;
+        case 'RS':
+            echo "Rio Grande do Sul";
+            break;
+        case 'RO':
+            echo "Rondônia";
+            break;
+        case 'RR':
+            echo "Roraima";
+            break;
+        case 'SC':
+            echo "Santa Catarina";
+            break;
+        case 'SP':
+            echo "São Paulo";
+            break;
+        case 'SE':
+            echo "Sergipe";
+            break;
+        case 'TO':
+            echo "Tocantins";
+            break;
         default:
-        echo "O numero informado não é valido";
+        echo "A sigla informada não é valida";
         } 
 
 ?>
