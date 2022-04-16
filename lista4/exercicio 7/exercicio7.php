e9dbc901e7501acf527e2e00c4f29a646eb9fd5f
luiza <marilusabion2203@gmail.com>
2022-04-16 14:55:09 -0300

    exercicio 7

--- lista4/exercicio 7/exercicio7.php
+++ lista4/exercicio 7/exercicio7.php
@@ -3,17 +3,13 @@
         dias de vida ela possui. Considere sempre anos completos, e que um ano possui 365
         dias. */
 
-        $nome = $_POST ['nome']
-        $idade = $_POST ['estado'];
-
+        $nome = $_POST ['nome'];
+        $idade = $_POST ['idade'];
+        
         $dias = $idade * 365;
-
+        
         echo "Nome: " . $nome . "<br>";
         echo "Idade: " . $idade . "<br>";
-
-        echo $nome . "possui " . $dias . "de vida<br>";
-
-
-
+        echo $nome . " possui " . $dias . " dias de vida <br>";
 
 ?>
