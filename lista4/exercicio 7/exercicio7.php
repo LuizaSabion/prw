228ccf37b0540d2a7891f044e9a702d53592f3c6
luiza <marilusabion2203@gmail.com>
2022-04-16 14:38:07 -0300

    exercicio 7

---
+++ lista4/exercicio 7/exercicio7.php
@@ -0,0 +1,19 @@
+<?php
+        /* Escreva um algoritmo para ler o nome e a idade de uma pessoa, e exibir quantos
+        dias de vida ela possui. Considere sempre anos completos, e que um ano possui 365
+        dias. */
+
+        $nome = $_POST ['nome']
+        $idade = $_POST ['estado'];
+
+        $dias = $idade * 365;
+
+        echo "Nome: " . $nome . "<br>";
+        echo "Idade: " . $idade . "<br>";
+
+        echo $nome . "possui " . $dias . "de vida<br>";
+
+
+
+
+?>
