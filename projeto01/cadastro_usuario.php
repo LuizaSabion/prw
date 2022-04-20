a3b54c5677e26ca2df56b29f99056a23b2b9343e
luiza <marilusabion2203@gmail.com>
2022-04-20 11:41:23 -0300

    cadastro

--- projeto01/cadastro_usuario.php
+++ projeto01/cadastro_usuario.php
@@ -1,10 +1,21 @@
 <?php
+    include('conexao.php');
     $nome_usuario = $_POST['nome_usuario'];
     $email_usuario = $_POST['email_usuario'];
-    $fone_usuario = $_POST['fone_usuario'];
+    $telefone_usuario = $_POST['telefone_usuario'];
 
     echo "<p>Nome do usuario: " . $nome_usuario . "<br>";
     echo "E-mail: " . $email_usuario . "<br>";
-    echo "Telefone: " . $fone_usuario . "</p>";
+    echo "Telefone: " . $telefone_usuario . "</p>";
+
+    $sql = "INSERT INTO usuario (nome_usuario, email_usuario, telefone_usuario)
+            VALUES ('". $nome_usuario."',  '" .$email_usuario."', '".$telefone_usuario."')";
+    
+    $result = mysqli_query($con, $sql);
+    if ($result) {
+        echo "Dados cadastrados com sucesso!";
+    }
+    else 
+        echo "Erro ao tentar cadastrar dados no banco!".mysqli_error($con);
 
 ?>
